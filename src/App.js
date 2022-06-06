9abd86d66c982840e5994daa89bfec054d0ea326
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-06-06 16:55:01 +0530

    bag

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import "./App.css";
 // import Product from "./components/Products/Product/Product";
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-import Product from "./components/Products/Product/Product";
+
 import Navbar from "./components/Navbar/Navbar";
 function App() {
   
-//   useEffect(()=>{
-          
-//     const products = async() =>{
-      
-//        let r = await axios.get("http://localhost:8080/products")
-
-//        console.log(r.data)
-
-//     }
-
-//     products();
-
-//  },[]) 
 
   return (
     <div className="App">
-       home
+       
         <Navbar/>
-        <Product/>
- 
-         
+    
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
 // use react-router Link or NavLink
-const Link = <a />;
+// const Link = <a />;
 
 const Navbar = () => {
   return (
-    <div data-cy="navbar">
-      <Link data-cy="navbar-home-link"></Link>
-      <span data-cy="navbar-cart-items-count">{/* count here */}</span>
-      <button data-cy="navbar-login-logout-button"></button>
+    <div data-cy="navbar" style={{display:"flex",justifyContent:"space-between"}}>
+     <div> <Link to="#" data-cy="navbar-home-link">Logo</Link></div>
+     <div>
+     <span data-cy="navbar-cart-items-count">count:0</span> &nbsp;
+      <button data-cy="navbar-login-logout-button">Login</button>
+     </div>
     </div>
   );
 };
